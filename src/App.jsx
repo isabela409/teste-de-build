237c517df3f3f7622495e0482fcb836191d81c32
isabela409 <isabelaalvesdasilva458@gmail.com>
2025-10-17 12:24:38 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Aula de implantação</h1>
+      <img src="https://static.vecteezy.com/ti/fotos-gratis/t2/36324708-ai-gerado-cenario-do-uma-tigre-caminhando-dentro-a-floresta-foto.jpg" alt="tigre" />
+    </>
+  )
+}
+
+export default App
